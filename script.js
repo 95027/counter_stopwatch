536055a84778ab452c7ad7f53d7b148e7ba91e83
Ch. Sai Kumar <96567025+95027@users.noreply.github.com>
2023-07-21 13:46:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+//counter
+
+const heading = document.querySelector("h1");
+const minus = document.querySelector(".minus");
+const reset = document.querySelector(".reset");
+const plus = document.querySelector(".plus");
+
+let initialCount = 0;
+
+
+minus.addEventListener("click", ()=>{
+  if(initialCount > 0){
+    initialCount-- ;
+    heading.innerHTML = "count " + initialCount;
+  }
+})
+
+plus.addEventListener("click", ()=>{
+  initialCount++;
+  heading.innerHTML = "count " + initialCount;
+})
+
+reset.addEventListener("click", ()=>{
+  initialCount = 0;
+  heading.innerHTML = "count " + initialCount;
+})
+
+
+
+//stop watch
+
+const title = document.querySelector(".title");
+const start = document.querySelector(".start");
+const resetsw = document.querySelector(".reset-sw");
+const stop = document.querySelector(".stop");
+
+
+let sw =0;
+var stopwatch= null;
+
+function started(){
+  sw++;
+  title.innerHTML = "stopwatch " + sw;
+} 
+
+
+
+start.addEventListener("click", ()=>{
+  started(); 
+});
+
+function started(){
+  stopwatch =
+  setInterval(()=>{
+    sw++;
+    title.innerHTML = "stopwatch " + sw;
+  },1000);
+}
+
+stop.addEventListener("click", ()=>{
+  clearInterval(stopwatch); 
+});
+
+resetsw.addEventListener("click", ()=>{
+  clearInterval(stopwatch);
+  stopwatch = null ;
+  sw = 0;
+  title.innerHTML = "stopwatch " + sw;
+})
+
